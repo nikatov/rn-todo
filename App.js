38fb684f5d64652746c2b1669849dd38b3cf3ffb
Владислав Никатов <nikatov@ivk.ru>
2021-05-26 14:03:18 +0300

    добавление альбомной ориентации, обновление верстки при ее изменении

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import AppLoading from 'expo-app-loading';
 import { Navbar } from './src/components/Navbar';
 import { MainScreen } from './src/screens/MainScreen';
 import { TodoScreen } from './src/screens/TodoScreen';
+import { THEME } from './src/theme';
 
 async function loadApplication() {
   await Font.loadAsync({
@@ -111,7 +112,7 @@ export default function App() {
 
 const styles = StyleSheet.create({
   mainWindow: {
-    paddingHorizontal: 16,
+    paddingHorizontal: THEME.PADDING_HORIZONTAL,
     paddingVertical: 8
   }
 });
--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,24 +1,39 @@
-import React from 'react';
-import {StyleSheet, View, FlatList, Image} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import {StyleSheet, View, FlatList, Image, Dimensions} from 'react-native';
 import {AddTodo} from '../components/AddTodo';
 import {Todo} from '../components/Todo';
+import { THEME } from '../theme';
+
 
 export const MainScreen = ({addTodo, removeTodo, todos, openTodo}) => {
+    const [deviceWidth, setDeviceWidth] = useState(Dimensions.get('window').width - 2 * THEME.PADDING_HORIZONTAL);
+    useEffect(() => {
+        const update = () => {
+            const width = Dimensions.get('window').width - 2 * THEME.PADDING_HORIZONTAL;
+            setDeviceWidth(width);
+        }
+        Dimensions.addEventListener('change', update);
+        return () => {
+            Dimensions.removeEventListener('change', update);
+        }
+    });
 
-    let content =            
-        <FlatList
-            keyExtractor={item => item.id.toString()}
-            data={todos}
-            renderItem={({item}) => {
-                return (
-                    <Todo
-                        todo={item}
-                        onRemove={removeTodo}
-                        onOpen={openTodo}
-                    />
-                    );
-                }}
-        />
+    let content =
+        <View style={{width: deviceWidth}}>
+            <FlatList
+                keyExtractor={item => item.id.toString()}
+                data={todos}
+                renderItem={({item}) => {
+                    return (
+                        <Todo
+                            todo={item}
+                            onRemove={removeTodo}
+                            onOpen={openTodo}
+                        />
+                        );
+                    }}
+            />
+        </View>
     if (todos.length === 0) {
         content =
             <View style={styles.imgWrap}>
