284a6b03c1a066230b51012e8a023944d253dee1
Владислав Никатов <nikatov@ivk.ru>
2021-05-28 17:39:21 +0300

    Создание редьюсера

--- src/context/todo/ToDoState.js
+++ src/context/todo/ToDoState.js
@@ -2,6 +2,7 @@ import React, { useReducer  } from 'react';
 
 import { todoReducer } from './todoReducer';
 import { TodoContext } from './todoContext';
+import { ADD_TODO, REMOVE_TODO, UPDATE_TODO } from '../types';
 
 export const TodoState = ({children}) => {
     const initialState = {
@@ -12,10 +13,18 @@ export const TodoState = ({children}) => {
     }
     const [state, dispatch] = useReducer(todoReducer, initialState);
 
+    const addTodo = title => dispatch({type: ADD_TODO, title});
+    const removeTodo = id => dispatch({type: REMOVE_TODO, id});
+    const updateTodo = (id, title ) => dispatch({type: UPDATE_TODO, id, title});
+
     return (
         <TodoContext.Provider
-        value={{
-            todos: state.todos
+        value=
+            {{
+                todos: state.todos,
+                addTodo,
+                removeTodo,
+                updateTodo
             }}
         >
             {children}
--- src/context/todo/todoReducer.js
+++ src/context/todo/todoReducer.js
@@ -1,3 +1,25 @@
+import { ADD_TODO, REMOVE_TODO, UPDATE_TODO } from "../types";
+
 export const todoReducer = (state, action) => {
+    switch (action.type) {
+        case ADD_TODO: {
+            const newTodo = {
+                id: Date.now().toString(),
+                title: action.title
+              };
+            return {...state, todos: [...state.todos, newTodo] }
+        }
+        case REMOVE_TODO:
+            return {...state, todos: state.todos.filter(todo => todo.id !== action.id)}
+        case UPDATE_TODO:
+            return {...state, todos: state.todos.map(todo => {
+                if (todo.id === action.id) {
+                    todo.title = action.title;
+                }
+                return todo;
+            })}
+        default:
+            break;
+    }
     return state;
 }
